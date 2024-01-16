1d05ba13477f6eabc98bdec1465e9caf7216dd69
gyusungjung <147012985+gyusungjung@users.noreply.github.com>
2024-01-16 17:46:43 +0900

    test1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,28 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { UserModule } from "./user/user.module";
-import { AuthModule } from "./auth/auth.module";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { ConfigProjectModule } from "./config/config.module";
-import { TypeormModule } from "./typeorm/typeorm.module";
-import { StoreModule } from "./store/store.module";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigProjectModule } from './config/config.module';
+import { TypeormModule } from './typeorm/typeorm.module';
+import { StoreModule } from './store/store.module';
 import { BookreviewModule } from './bookreview/bookreview.module';
-import { MenuModule } from "./menu/menu.module";
+import { MenuModule } from './menu/menu.module';
 
 @Module({
-    imports: [
-        ConfigModule,
-        ConfigProjectModule,
-        TypeormModule.forRoot(),
-        UserModule,
-        AuthModule,
-        StoreModule,
-        BookreviewModule,
-        MenuModule,
-    ],
-    controllers: [AppController],
-    providers: [AppService, ConfigService],
-    exports: [ConfigService],
+  imports: [
+    ConfigModule,
+    ConfigProjectModule,
+    TypeormModule.forRoot(),
+    UserModule,
+    AuthModule,
+    StoreModule,
+    BookreviewModule,
+    MenuModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService, ConfigService],
+  exports: [ConfigService],
 })
 export class AppModule {}
