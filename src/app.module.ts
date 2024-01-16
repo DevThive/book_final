e5deaef3e312f6a36f51919e4aabd5195d906de8
gyusungjung <wjdrbtjd92@gmail.com>
2024-01-16 16:53:41 +0900

    bookreview 일단푸시

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
 import { ConfigProjectModule } from "./config/config.module";
 import { TypeormModule } from "./typeorm/typeorm.module";
 import { StoreModule } from "./store/store.module";
+import { BookreviewModule } from './bookreview/bookreview.module';
 
 @Module({
     imports: [
@@ -16,6 +17,7 @@ import { StoreModule } from "./store/store.module";
         UserModule,
         AuthModule,
         StoreModule,
+        BookreviewModule,
     ],
     controllers: [AppController],
     providers: [AppService, ConfigService],
