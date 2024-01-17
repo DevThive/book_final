63bb0c3a9e30fae3248ac2062559f8a363741d12
gyusungjung <147012985+gyusungjung@users.noreply.github.com>
2024-01-17 13:22:51 +0900

    최신화

--- src/bookreview/bookreview.controller.ts
+++ src/bookreview/bookreview.controller.ts
@@ -7,7 +7,6 @@ import {
   Put,
   Delete,
   UseGuards,
-  Request,
 } from '@nestjs/common';
 import { BookReviewService } from './bookreview.service';
 import { CreateBookReviewDto } from './dto/create-bookreview.dto';
@@ -21,21 +20,24 @@ export class BookReviewController {
 
   @UseGuards(accessTokenGuard)
   @Post()
+
+  //리뷰작성
   create(
     @Param('bookId') book_id: number,
-    @Request() req: any,
+    @UserId() userId: number,
     @Body() createBookReviewDto: CreateBookReviewDto,
   ) {
-    const userId = req.user.userId; //
     return this.bookReviewService.create(book_id, userId, createBookReviewDto);
   }
 
+  //특정책 전체조회
   @Get()
   findAll(@Param('bookId') book_id: number = 1) {
     // 1로 현재 디폴트를 준 상태
     return this.bookReviewService.findAll(book_id);
   }
 
+  //특정책 특정리뷰조회
   @Get(':id')
   findOne(@Param('bookId') book_id: number, @Param('id') id: number) {
     return this.bookReviewService.findOne(book_id, id);
@@ -49,7 +51,7 @@ export class BookReviewController {
     @UserId() userId: number,
     @Body() updateBookReviewDto: UpdateBookReviewDto,
   ) {
-    return this.bookReviewService.update(
+    return this.bookReviewService.updateBookReview(
       book_id,
       id,
       userId,
@@ -62,9 +64,8 @@ export class BookReviewController {
   remove(
     @Param('bookId') book_id: number,
     @Param('id') id: number,
-    @Request() req: any,
+    @UserId() userId: number,
   ) {
-    const userId = req.user.userId; // JWT AccessToken에서 userId 가져오기
-    return this.bookReviewService.remove(book_id, id, userId);
+    return this.bookReviewService.deleteBookReview(book_id, id, userId);
   }
 }
